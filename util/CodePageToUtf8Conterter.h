ba8779b925f974a5da71cb7c46022e17fa510934
Rui Pires <rui@sennin.pt>
2021-01-17 22:55:19 -0100

    Conversion to UTF-16 (WIP)

---
+++ util/CodePageToUtf8Conterter.h
@@ -0,0 +1,13 @@
+#ifndef BSTY_CODEPAGETOUTF8CONTERTER_H
+#define BSTY_CODEPAGETOUTF8CONTERTER_H
+
+#include <string>
+
+class CodePageToUtf8Conterter
+{
+    public:
+        std::string fromCp1252(std::string const& in);
+};
+
+
+#endif
