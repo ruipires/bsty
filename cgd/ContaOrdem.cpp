898983cdc09cd94a869ab3d1b6553052bb6002e0
Rui Pires <rui@sennin.pt>
2021-04-22 07:49:37 +0000

    Initial usage of date library.

--- cgd/ContaOrdem.cpp
+++ cgd/ContaOrdem.cpp
@@ -1,9 +1,13 @@
 #include "ContaOrdem.h"
+#include "util/CodePageToUtf8Converter.h"
+
 #include <spdlog/spdlog.h>
 #include <fast-cpp-csv-parser/csv.h>
+#include <date/date.h>
+
 #include <fstream>
 #include <sstream>
-#include "util/CodePageToUtf8Converter.h"
+
 
 namespace
 {
@@ -44,16 +48,30 @@ void cgd::ContaOrdem::load_csv(std::string const& filename, std::istream &in_fil
 {
     std::string tmp;
     for(int i = 0; i != 6; ++i)
+    {
         std::getline(in_file, tmp);
+        spdlog::trace("Skipping over line #{}, contents:\"{}\"", i, tmp);
+    }
 
-    io::CSVReader<8> in(filename, in_file);
+    //io::CSVReader<8> in(filename, in_file);
+    io::CSVReader<8, io::trim_chars<' ', '\t'>, io::no_quote_escape<';'>> in(filename, in_file);;
     in.read_header(io::ignore_extra_column, "Data mov.", "Data valor", "Descrição", "Débito", "Crédito", "Saldo contabilístico", "Saldo disponível", "Categoria");
 
-    char * raw_date_mov, raw_date_val;
-    std::string desc, outflow, inflow, balance, available_balance, category;
+    std::string raw_date_mov, raw_date_val, desc, outflow, inflow, balance, available_balance, category;
 
     while(in.read_row(raw_date_mov, raw_date_val, desc, outflow, inflow, balance, available_balance, category))
     {
-        spdlog::trace("* row {{ date='{}', payee='{}', memo='{}' outflow='{}', inflow='{}' }}");
+        spdlog::trace(
+                "* row {{ date_mov='{}', date_val='{}', desc='{}' outflow='{}', inflow='{}', balance='{}', available_balance='{}', category='{}' }}",
+                raw_date_mov, raw_date_val, desc, outflow, inflow, balance, available_balance, category);
+
+        // parse dates - https://stackoverflow.com/a/41613816
+
+        std::istringstream date{raw_date_mov};
+        date::sys_time<date::days> tp;
+        date >> date::parse("%d-%m-%Y", tp);
+        date::year_month_day x{tp};
+        spdlog::trace("** raw_date={} => year:{} month:{} day:{}", raw_date_mov, int(x.year()), unsigned(x.month()),
+                      unsigned(x.day()));
     }
 }
