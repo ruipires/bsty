8ebd5e47c551f38d3ff68a77fdca6962c07ba33d
Rui Pires <rui@sennin.pt>
2021-04-22 22:03:52 +0000

    introduces simple date parser wrapper class.

---
+++ util/DateParser.cpp
@@ -0,0 +1,28 @@
+#include "DateParser.h"
+#include <date/date.h>
+
+DateParser::DateParser(char const *formatString)
+:formatString_(formatString)
+{
+}
+
+std::optional<DateParser::YearMonthDay> DateParser::parse(std::string const &inputData)
+{
+    std::optional<YearMonthDay> result;
+
+    std::istringstream date{inputData};
+    date::sys_time<date::days> tp;
+    date >> date::parse(formatString_, tp);
+    date::year_month_day x{tp};
+
+    if(date)
+    {
+        YearMonthDay ymd;
+        ymd.year = static_cast<int>(x.year());
+        ymd.month = static_cast<unsigned int>(x.month());
+        ymd.day= static_cast<unsigned int>(x.day());
+        result = ymd;
+    }
+
+    return result;
+}
---
+++ util/DateParser.h
@@ -0,0 +1,32 @@
+#ifndef BSTY_DATEPARSER_H
+#define BSTY_DATEPARSER_H
+
+#include <optional>
+#include <string>
+
+class DateParser
+{
+    public:
+        struct YearMonthDay
+        {
+            YearMonthDay()
+                :year(0)
+                ,month(0)
+                ,day(0)
+            {}
+
+            int year;
+            unsigned int month;
+            unsigned int day;
+        };
+
+        explicit DateParser(char const* formatString="%d-%m-%Y");
+
+        std::optional<YearMonthDay> parse(std::string const& inputData);
+
+    private:
+        std::string const formatString_;
+};
+
+
+#endif
